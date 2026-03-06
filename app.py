0d9bc90224cb132b4ca35945b42e313740c9294b
nova-07-07-07 <snova234607ns@gmail.com>
2026-03-06 14:04:26 +0530

    Add status route to return server status

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "server running"
+    
+@app.route("/status")
+def status():
+    return "Server Running Perfectly"
 
 if __name__ == "__main__":
     app.run(port=5123)
